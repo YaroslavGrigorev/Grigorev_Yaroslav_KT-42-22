f99d7b446881a6b8881654f23775bf59cc78b89f
Yaroslav Grigorev <grigyroslav@gmail.com>
2025-03-10 13:52:23 +0300

    Вторая лабораторная работа

--- Project_practicum/Controllers/WeatherForecastController.cs
+++ Project_practicum/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace Project_practicum.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
@@ -28,6 +29,16 @@ namespace Project_practicum.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpPost(Name = "AddNewSummary")]
+        public string[] AddNewSummary(string newSummary) 
+        {
+            _logger.LogError("New method was called");
+
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
         }
     }
 }
